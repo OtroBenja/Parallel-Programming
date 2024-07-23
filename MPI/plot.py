308d109b585650a8da1ebf5e155489ab689a9d32
OtroBenja <135280570+OtroBenja@users.noreply.github.com>
2024-07-22 20:39:32 -0400

    Update plot.py

--- MPI/plot.py
+++ MPI/plot.py
@@ -1,4 +1,3 @@
-#Sequential run
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,7 +9,8 @@ ax.axis('off')
 fig.tight_layout()
 fig.savefig('Rho_field.png')
 
-dataField = np.loadtxt('Phi_solution.csv',delimiter=',')
+date = '_16:15:15'
+dataField = np.loadtxt('Phi_solution'+date+'.csv',delimiter=',')
 fig, ax = plt.subplots(1,figsize=(12,6),dpi=100)
 plot1 = ax.matshow(dataField)
 fig.colorbar(plot1)
