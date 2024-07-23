6eddc273f17a1a7c84065649a93ca7eff49ad5e5
OtroBenja <135280570+OtroBenja@users.noreply.github.com>
2024-07-22 20:11:30 -0400

    Update jacobi_mpi.c
    print log file and add time to filename

--- MPI/jacobi_mpi.c
+++ MPI/jacobi_mpi.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 void printOutput(int rows, int cols, float data[]){
@@ -10,6 +11,43 @@ void printOutput(int rows, int cols, float data[]){
         printf("%f ",data[i]);
         if ((i+1)%cols == 0) printf("\n");
     }
+}
+
+void fprint_field(int rows, int cols, float* field, char *filename){
+    char strBuff[50];
+    snprintf(strBuff, sizeof(strBuff), "%s.csv", filename);
+    FILE* values_file = fopen(strBuff,"w");
+    for(int i=0;i<rows-1;i++){
+        for(int j=0;j<cols-1;j++){
+            fprintf(values_file,"%f,",field[i*cols +j]);
+        }
+        fprintf(values_file,"%f\n",field[i*cols +cols-1]);
+    }
+    for(int j=0;j<cols-1;j++)
+        fprintf(values_file,"%f,",field[cols*(rows-1) +j]);
+    fprintf(values_file,"%f",field[rows*cols-1]);
+    fclose(values_file);
+}
+
+void fprint_log(int numP, float execTime, int numIter, float finalError, char *date){
+    char strBuff[50];
+    snprintf(strBuff, sizeof(strBuff), "Log_%s.txt", date);
+    FILE* values_file = fopen(strBuff,"w");
+
+    printf("Iteration type: MPI 1D\nNumber of processes: %d\nTotal time: %f"
+        "\nTotal iterations: %d\nFinal error: %f",numP,execTime,numIter,finalError);
+}
+
+void functionRho(int rows,int cols,float *rho){
+    for(int i=0;i<rows*cols;i++){
+        rho[i] = 0;
+    }
+    rho[cols*(rows/3  ) +cols/6   ] =  1;
+    rho[cols*(rows/3  ) +cols*7/12] =  1;
+    rho[cols*(rows*2/3) +cols/6   ] =  1;
+    rho[cols*(rows*2/3) +cols*7/12] =  1;
+    rho[cols*(rows/2  ) +cols*5/12] = -2;
+    rho[cols*(rows/2  ) +cols*5/6 ] = -2;
 }
 
 void readInput(int rows, int cols, float data[]){
@@ -76,10 +114,13 @@ int main (int argc, char *argv[]){
     }
 
     float *data;
+    float *rho;
 
     if(myID == 0){
         data = (float*) malloc( rows*cols*sizeof(float));
+        rho = (float*) malloc( rows*cols*sizeof(float));
         readInput(rows, cols, data);
+        functionRho(rows, cols, rho);
     }
 
     // The computation is divided by rows
@@ -92,10 +133,12 @@ int main (int argc, char *argv[]){
 
     // Arrays for the chunk of data
     float *myData = (float*) malloc( myRows*cols*sizeof(float));
+    float *myRho = (float*) malloc( myRows*cols*sizeof(float));
     float *buff = (float*) malloc( myRows*cols*sizeof(float)); // Auxiliary array
 
     // Scatter the input matrix
     MPI_Scatter(data, myRows*cols, MPI_FLOAT, myData, myRows*cols, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(rho, myRows*cols, MPI_FLOAT, myRho, myRows*cols, MPI_FLOAT, 0, MPI_COMM_WORLD);
     memcpy(buff, myData, myRows*cols*sizeof(float));
 
     float error = errThres + 1.0;
@@ -105,6 +148,8 @@ int main (int argc, char *argv[]){
     float *prevRow = (float*) malloc( cols*sizeof(float));
     float *nextRow = (float*) malloc( cols*sizeof(float));
 
+    //Register the number of iterations
+    int iter = 0;
     while (error > errThres){
 	    if (myID > 0){
 	        // Send first row to previous process
@@ -123,7 +168,7 @@ int main (int argc, char *argv[]){
 	    // Update the first row
 	    if ((myID > 0) && (myRows > 1)){
 	        for (int j=1; j < cols-1; j++){
-	            buff[j] = 0.25f*(myData[cols+j]+myData[j-1]+myData[j+1]+prevRow[j]);
+	            buff[j] = 0.25f*(myData[cols+j]+myData[j-1]+myData[j+1]+prevRow[j]-myRho[j]);
 	        }
 	    }
 
@@ -131,14 +176,14 @@ int main (int argc, char *argv[]){
 	    for (int i=1; i < myRows-1; i++){
 	        for (int j=1; j < cols-1; j++){
 	            // calculate discrete Laplacian with 4-point stencil
-	            buff[i*cols+j] = 0.25f*(myData[(i+1)*cols+j]+myData[i*cols+j-1]+myData[i*cols+j+1]+myData[(i-1)*cols+j]);
+	            buff[i*cols+j] = 0.25f*(myData[(i+1)*cols+j]+myData[i*cols+j-1]+myData[i*cols+j+1]+myData[(i-1)*cols+j]-myRho[i*cols+j]);
 	        }
 	    }
 
 	    // Update the last row
 	    if ((myID < numP-1) && (myRows > 1)){
 	        for (int j=1; j < cols-1; j++){
-	            buff[(myRows-1)*cols+j] = 0.25f*(nextRow[j]+myData[(myRows-1)*cols+j-1]+myData[(myRows-1)*cols+j+1]+myData[(myRows-2)*cols+j]);
+	            buff[(myRows-1)*cols+j] = 0.25f*(nextRow[j]+myData[(myRows-1)*cols+j-1]+myData[(myRows-1)*cols+j+1]+myData[(myRows-2)*cols+j]-myRho[(myRows-1)*cols+j]);
 	        }
 	    }
 
@@ -149,6 +194,7 @@ int main (int argc, char *argv[]){
 	            // Determine difference between data and buff
 	            myError += (myData[i*cols+j]-buff[i*cols+j])*(myData[i*cols+j]-buff[i*cols+j]);
 	        }
+        iter++;
 	    }
 
     memcpy(myData, buff, myRows*cols*sizeof(float));
@@ -166,9 +212,19 @@ int main (int argc, char *argv[]){
     double end = MPI_Wtime();
 
     if (myID == 0){
-        printf("Time with %d processes: %f seconds.\n",numP,end-start);
-        printf("Final error: %f\n",error);
-        //printOutput(rows, cols, data);
+        //printf("Time with %d processes: %f seconds.\n",numP,end-start);
+        //printf("Final error: %f\n",error);
+        //Print final results to a file
+        char *fname = "Phi_solution";
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        char date[20];
+        snprintf(date, sizeof(date), "%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+        char finalFname[40];
+        snprintf(finalFname, sizeof(finalFname), "%s_%s", fname, date);
+        fprint_field(rows, cols, data, finalFname);
+        //Make log file with data about the execution
+        fprint_log(numP,end-start,iter,error,date);
         free(data);
     }
 
