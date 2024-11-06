ce4b5570250a7333386c97dee235dbfc7fadef9a
OtroBenja <135280570+OtroBenja@users.noreply.github.com>
2024-11-06 20:42:09 -0300

    Added antialiasing and camera settings
    camera object now has position and focal_length as global parameters that can be adjusted from main or other functions

--- Ray-Tracing/main.cpp
+++ Ray-Tracing/main.cpp
@@ -9,14 +9,14 @@ int main() {
 
     hittable_list world;
 
-    //world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-    world.add(make_shared<sphere>(point3(0,1.5,-1), 1.6));
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width  = 400;
+    cam.samples_per_pixel = 50;
 
     cam.render(world);
 
