69c4fbc3ebd000b9bb927eeb000b70a58e1ea6bc
OtroBenja <benja.gra2002@gmail.com>
2024-11-07 14:34:07 -0300

    Update libraries and main
    the main and libraries are updated up to the fuzzy metals section in the book

--- Ray-Tracing/camera.h
+++ Ray-Tracing/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     public:
@@ -9,8 +10,10 @@ class camera {
         double aspect_ratio      = 1.0; // Ratio of image width over height
         int    image_width       = 100; // Rendered image width in pixel count
         int    samples_per_pixel = 10;  // Count of random samples for each pixel
+        int    max_depth         = 10;  // Maximum number of ray bounces into scene
         point3 center            = point3(0, 0, 0); // Camera center
-        double focal_length      = 1.0;
+        double focal_length      = 1.0; // Focal length of the camera
+        color sky_color          = color(0.5, 0.7, 1.0); // Base color of the skybox
 
         void render(const hittable& world) {
             initialize();
@@ -23,7 +26,7 @@ class camera {
                     color pixel_color(0,0,0);
                     for (int sample = 0; sample < samples_per_pixel; sample++) {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
@@ -87,16 +90,25 @@ class camera {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
+            // If we've exceeded the ray bounce limit, no more light is gathered.
+            if (depth <= 0)
+                return color(0,0,0);
+
             hit_record rec;
 
-            if (world.hit(r, interval(0, infinity), rec)) {
-                return 0.5 * (rec.normal + color(1,1,1));
+            if (world.hit(r, interval(0.001, infinity), rec)) {
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered))
+                    return attenuation * ray_color(scattered, depth-1, world);
+                return color(0,0,0);
+                //return 0.5 * (rec.normal + color(1,1,1)); //Normal map
             }
 
             vec3 unit_direction = unit_vector(r.direction());
             auto a = 0.5*(unit_direction.y() + 1.0);
-            return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+            return (1.0-a)*color(1.0, 1.0, 1.0) + a*sky_color;
         }
 };
 
--- Ray-Tracing/octavarium.cpp
+++ Ray-Tracing/octavarium.cpp
@@ -9,13 +9,17 @@ int main() {
 
     hittable_list world;
 
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.9, 0.1));
+    auto material_pendulum = make_shared<metal>(color(0.7, 0.7, 0.7), 0.6);
+
     double deltaX = 0.1;
 
-    world.add(make_shared<sphere>(point3(0  -deltaX,0,-1)  , 0.5));
-    world.add(make_shared<sphere>(point3(-1 -deltaX,0,-1) , 0.5));
-    world.add(make_shared<sphere>(point3(-2 -deltaX,0,-1) , 0.5));
-    world.add(make_shared<sphere>(point3(1.65-deltaX,0.3,-1), 0.5));
-    world.add(make_shared<sphere>(point3(22,-199.7,-1), 200));
+    world.add(make_shared<sphere>(point3(0  -deltaX ,0     ,-1), 0.5,material_pendulum));
+    world.add(make_shared<sphere>(point3(-1 -deltaX ,0     ,-1), 0.5,material_pendulum));
+    world.add(make_shared<sphere>(point3(-2 -deltaX ,0     ,-1), 0.5,material_pendulum));
+    world.add(make_shared<sphere>(point3(1.65-deltaX,0.3   ,-1), 0.5,material_pendulum));
+    world.add(make_shared<sphere>(point3(22         ,-199.7,-20), 200,material_ground  ));
 
     camera cam;
 
@@ -25,6 +29,8 @@ int main() {
     cam.aspect_ratio = 1.0;
     cam.image_width  = 400;
     cam.samples_per_pixel = 50;
+    cam.max_depth = 100;
+    cam.sky_color = color(0.2, 0.2, 1.0);
 
     cam.render(world);
 
